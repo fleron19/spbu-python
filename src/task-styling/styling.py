565f0639847f76a4a7eebfdf8e594ee18c948f46
Mikhail Romanovskiy <romanovskymv@gmail.com>
2025-10-13 15:30:51 +0300

    Stylize function name to snake_case

--- src/task-styling/styling.py
+++ src/task-styling/styling.py
@@ -1,10 +1,10 @@
-def extendedEuclidean(a, b):
+def extended_euclidean(a, b):
     if a == 0:
         return (b, 0, 1)
-    d = extendedEuclidean(b % a, a) # Рекурсивно применяем алгоритм Евклида
+    d = extended_euclidean(b % a, a) # Рекурсивно применяем алгоритм Евклида
     x = d[2] - (b // a) * d[1] # Преобразуем для получения линейного представления
     y = d[1]
     return (d[0], x, y) 
-# Возвращаем кортеж из трех чисел, где первое = gcd, второе = коэф. первого числа, третье = коэф второго числа
-print(extendedEuclidean(12, 4))
-print(extendedEuclidean(25, 15))
+# Возвращаем кортеж из трех чисел, где первое = gcd, второе = коэф. первого числа, третье = коэф. второго числа
+print(extended_euclidean(12, 4))
+print(extended_euclidean(25, 15))
