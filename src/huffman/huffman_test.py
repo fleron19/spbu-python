2ab4857a55a3a97d2f9d9f3bfb68a0dfab46d45f
Mikhail Romanovskiy <romanovskymv@gmail.com>
2025-11-16 23:41:24 +0300

    Minor fixes and repeating data test

--- src/huffman/huffman_test.py
+++ src/huffman/huffman_test.py
@@ -19,6 +19,16 @@ def test_file_code():
     with open("random", "wb") as fout:
         fout.write(os.urandom(size)) 
 
-        encode_file("random")
-        decode_file("random_encoded", "res")
-        assert filecmp.cmp("random", "res")
+    encode_file("random")
+    decode_file("random_encoded", "res")
+    assert filecmp.cmp("random", "res") # Encoded file is bigger because data is random
+
+def test_zeros_file_code():
+    num = 10000
+    with open("zeros", "wb") as fout:
+        fout.write(b'0' * num) 
+
+    encode_file("zeros")
+    decode_file("zeros_encoded", "zeros_res")
+    assert filecmp.cmp("zeros", "zeros_res") # Encoded file is 7 times smaller because data is repeating
+
